6fd9eefb8d03ff1bd10c232044026e4b30ba69c4
rurumimic <10915235+rurumimic@users.noreply.github.com>
2023-11-21 20:32:59 +0900

    sort::bubble_sort #2
    - check swap / sorted

--- src/sort/bubble_sort.rs
+++ src/sort/bubble_sort.rs
@@ -5,12 +5,18 @@ pub fn bubble_sort<T: Ord + Display>(arr: &mut [T]) {
         return;
     }
 
-    for i in 0..arr.len() {
-        for j in i..arr.len()-i-1 {
-            if arr[j] > arr[j+1] {
-                arr.swap(j, j+1);
+    let mut sorted = false;
+    let mut n = arr.len();
+
+    while !sorted {
+        sorted = true;
+        for i in 0..n - 1 {
+            if arr[i] > arr[i + 1] {
+                arr.swap(i, i+1);
+                sorted = false;
             }
         }
+        n -= 1;
     }
 }
 
